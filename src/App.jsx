82a48da515155e60c8c514d9e4b2ca91dd3d15d8
Daniel <danielmilevsky@gmail.com>
2025-12-17 18:00:23 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300 overflow-hidden">
+
+   </div>
+  )
+}
+
+export default App

9855d8eb03adbda1658f283075b1466dc61e4f08
Daniel <danielmilevsky@gmail.com>
2025-12-22 19:23:33 -0500

    added custom scrollbar, changed some colors and spacing

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -16,11 +16,72 @@ export default function AboutMe() {
                             ${TabMode === "AboutMe" ? "TabBlurIn" : "TabBlurOut"}`}>
             </div>
             <div className={`z-20 lg:w-[90vw] w-full h-[90vh] mt-3 rounded-2xl pt-[10vh]
-                            ${darkMode ? "bg-dr2Green/80" : "bg-dfGray/80"}
+                            ${darkMode ? "bg-drSecondaryDark/80" : "bg-dfSecondary/80"}
                             ${TabMode === "AboutMe" ? "TabIn" : "TabOut"}`}>
-                <div className="px-4 pt-3 h-full bg-ltGray/50 rounded-b-xl text-xl atkinson-hyperlegible-regular">
+                <div className={`px-4 pt-3 max-h-[80vh] rounded-b-xl text-xl atkinson-hyperlegible-regular overflow-y-auto scrollbar-thin scrollbar-webkit resize-none 
+                            ${darkMode ? "bg-dr2SecondaryDark/80 text-lt2PrimaryDark/80" : "bg-ltSecondary/50 text-black"}`}>
                     {/* <h2 className="text-3xl text-center mb-3">About me</h2> */}
                     <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore rem quisquam
+                        tenetur ullam, eveniet cumque, fugiat dicta laborum repellendus unde,|
+                        consequuntur nam similique dolor fugit repellat quasi enim voluptates quod!
 
                     </p>
                 </div>
